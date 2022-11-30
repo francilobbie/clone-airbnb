2349e72dbd8a46151075084f0c7f5f1c1f6e6334
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-11-30 08:38:56 +0100

    flats's index view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :flats, only: :index do
+    resources :users, only: :show
+  end
 end
