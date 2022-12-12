d701c4495d0eb25314bd1649b402c100c23dc7c2
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-12-12 18:58:41 +0100

    airbnb clone ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
 class UsersController < ApplicationController
 
-  def show
 
+  def show
+    @user = User.find(params[:id])
+    @tombstones = @user.tombstones
+    @markers = @tombstones.map do |tombstonemap|
+      {
+        lat: tombstonemap.latitude,
+        lng: tombstonemap.longitude
+      }
+    end
+  end
+
+  def my_missions
+    @orders = Order.where(user: current_user)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      head :ok
+      redirect_to user_path(@user)
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :avatar, :email, :encrypted_password)
   end
 end
