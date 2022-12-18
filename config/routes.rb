ce71ed25ea929f8d8b98304e7e1220d83e3bf219
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-12-18 03:18:16 +0100

    app init

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :flats, only: :index do
-    resources :users, only: :show
   end
+  resources :users, only: :show
 end
