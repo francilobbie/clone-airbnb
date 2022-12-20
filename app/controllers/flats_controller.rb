dd1a80e5956cb8a85b2b5d73ff74a42c06613578
Franci-lobbie LALANE <francilobbie.lalane@gmail.com>
2022-12-20 10:36:43 +0100

    Update flats_controller.rb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,17 +4,7 @@ class FlatsController < ApplicationController
     if params[:query].present?
       @flats = Flat.where("title ILIKE ?", "%#{params[:query]}%")
     else
-    @flats = Flat.all
-    end
-    @flats = Flat.where.not(latitude: nil, longitude: nil)
-    @markers = @flats.map do |flat|
-      {
-        lat: flat.latitude,
-        lng: flat.longitude#,
-        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-        # Uncomment the above line if you want each of your markers to display a info window when clicked
-        # (you will also need to create the partial "/flats/map_box")
-      }
+      @flats = Flat.all
     end
   end
 
