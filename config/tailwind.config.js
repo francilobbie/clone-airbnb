b0cb56d27642d05702f5cec9747b7c9c7c893494
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2023-04-01 11:11:14 +0200

    init

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,43 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+// module.exports = {
+//   content: [
+//     './public/*.html',
+//     './app/helpers/**/*.rb',
+//     './app/javascript/**/*.js',
+//     './app/views/**/*.{erb,haml,html,slim}'
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//     require('@tailwindcss/aspect-ratio'),
+//     require('@tailwindcss/typography'),
+//     require('@tailwindcss/container-queries'),
+//   ]
+// }
+
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+  mode: 'jit',
+  purge: [
+    './app/**/*.html.erb',
+    './app/**/*.html.haml',
+    './app/**/*.html.slim',
+    './app/**/*.js',
+    './app/**/*.jsx',
+    './app/**/*.ts',
+    './app/**/*.tsx',
+    './app/**/*.scss',
   ],
+  darkMode: false,
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+  variants: {},
+  plugins: [],
+};
