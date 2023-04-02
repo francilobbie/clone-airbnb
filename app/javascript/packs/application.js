eb4ecd0f01f68bbd735dbff6c5ddf6704be79e21
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2023-04-02 13:23:07 +0200

    still no working

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,5 @@
+import "tailwindcss/base";
+import "tailwindcss/components";
+import "tailwindcss/utilities";
+// app/javascript/packs/application.js
+import "../stylesheets/application";
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/**/*.html',
+    './app/**/*.erb',
+    './app/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
